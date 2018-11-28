0d1f8bdfeb9e7e9a7ec7df990f42bf6d4f88e548
Órion Winter <orion.winter66@gmail.com>
2018-11-28 03:32:51 -0300

    Cria dado sobre genero de domicílios

--- lab3/data_wrangling.R
+++ lab3/data_wrangling.R
@@ -1,19 +1,19 @@
-library(dplyr)
+library(tidyverse)
 # filtra município joão pessoa
 
-censo.pb <- read.csv("data/basico-censo-pb.csv")
+censo.pb <- read_csv("data/basico-censo-pb.csv")
 censo.joao.pessoa <- censo.pb %>% filter(Nome_do_municipio == "JOÃO PESSOA")
 
-write.csv(x = censo.joao.pessoa, file = "data/basico-censo-joao-pessoa.csv")
+write_csv(x = censo.joao.pessoa, "data/basico-censo-joao-pessoa.csv")
 
-domicilio <- read.csv("data/censo/Domicilio01_PB.csv", sep = ";", stringsAsFactors = F)
+domicilio <- read_csv2("data/censo/Domicilio01_PB.csv", na = "X")
 domicilio.genero <- domicilio %>% 
-  mutate_at(
-    c(64:70, 83:89),
-    as.numeric
-  ) %>% 
+  rowwise() %>% 
   mutate(
-    lider_masculino = V062 + V063 + V064 + V065 + V066 + V067 + V068,
-    lider_feminino = V081 + V082 + V083 + V084 + V085 + V086 + V087,
-    porcentagem_lideres_femininos = lider_feminino / (lider_feminino + lider_masculino)
+    lider_masculino = sum(V062, V063, V064, V065, V066, V067, V068, na.rm = TRUE),
+    lider_feminino = sum(V081, V082, V083, V084, V085, V086, V087, na.rm = TRUE),
+    porcentagem_lideres_femininos = (lider_feminino / (lider_feminino + lider_masculino)) * 100
   )
+
+write_csv(select(domicilio.genero, Cod_setor, lider_masculino, lider_feminino, porcentagem_lideres_femininos),
+          "data/domicilio_genero.csv")
